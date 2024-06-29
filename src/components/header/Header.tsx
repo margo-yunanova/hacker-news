983084c48d3de8171dac7189edd4d691b62307ae
Margo <m.yunanova@gmail.com>
2024-06-29 20:25:37 +0300

    fix: some fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,43 @@
+import { FC, useState } from 'react';
 import styles from './header.module.css';
 
-export const Header = () => {
+export type sortingType = 'new' | 'best' | 'top';
+
+interface IHeader {
+  handleSortButton: (type: sortingType) => void;
+  activeButton: 'new' | 'best' | 'top';
+}
+
+export const Header: FC<IHeader> = ({ handleSortButton, activeButton }) => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Hacker News</h1>
-      <div>
-        Сортировать: <button className={styles.button}>Новые | </button>
-        <button className={styles.button}> Лучшие | </button>
-        <button className={styles.button}> Топ</button>
+      <div className={styles['button-group']}>
+        Сортировать:
+        <button
+          onClick={() => {
+            handleSortButton('new');
+          }}
+          className={`${styles.button} ${activeButton === 'new' ? styles['active-button'] : ''}`}
+        >
+          Новые
+        </button>
+        <button
+          onClick={() => {
+            handleSortButton('best');
+          }}
+          className={`${styles.button} ${activeButton === 'best' ? styles['active-button'] : ''}`}
+        >
+          Лучшие
+        </button>
+        <button
+          onClick={() => {
+            handleSortButton('top');
+          }}
+          className={`${styles.button} ${activeButton === 'top' ? styles['active-button'] : ''}`}
+        >
+          Топ
+        </button>
       </div>
     </header>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const NEWS_API_URL = 'https://hacker-news.firebaseio.com/v0';
+
+export const localStorage = window.localStorage;
