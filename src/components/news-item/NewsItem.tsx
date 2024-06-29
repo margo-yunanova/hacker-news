f8ff8f0c764c70fe4e462788248f59fe50370585
Margo <m.yunanova@gmail.com>
2024-06-29 18:13:23 +0300

    feat: add news item component

---
+++ src/components/news-item/NewsItem.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import styles from './newsItem.module.css';
+
+export interface INewsItem {
+  serialNumber: number;
+  id: number;
+  by: string;
+  title: string;
+  score: number;
+  url: string;
+}
+
+export const NewsItem: FC<INewsItem> = ({
+  serialNumber,
+  id,
+  by,
+  title,
+  score,
+  url,
+}) => {
+  const domainName = url?.split('/').at(2);
+  return (
+    <li className={styles['list-item']}>
+      <span className={styles['serial-number']}>{serialNumber}</span>
+      <div className={styles.main}>
+        <a className={styles['title-block']}>
+          <h2 className={styles.title}>
+            {title} <span className={styles.subtitle}>({domainName})</span>
+          </h2>
+        </a>
+        <span className={styles.subtitle}>
+          {score} points by {by}
+        </span>
+      </div>
+    </li>
+  );
+};
