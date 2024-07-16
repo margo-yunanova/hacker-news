6282240343acffb7d9595bf1b71fea64cca20c05
Margo <m.yunanova@gmail.com>
2024-07-16 19:18:58 +0300

    fix: fix some styles

--- src/components/news-item/NewsItem.tsx
+++ src/components/news-item/NewsItem.tsx
@@ -20,7 +20,7 @@ export const NewsItem: FC<INewsItem> = ({
   const domainName = url?.split('/').at(2);
   return (
     <li className={styles['list-item']}>
-      <span className={styles['serial-number']}>{serialNumber}</span>
+      <span className={styles['serial-number']}>{serialNumber}.</span>
       <div className={styles.main}>
         <a className={styles['title-block']}>
           <h2 className={styles.title}>
