88b009e59c9202506391fc73b010d41aab50ec27
Margo <m.yunanova@gmail.com>
2024-07-19 19:58:36 +0300

    feat: implement infinity scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './App.module.css';
 import { NewsItem } from './components/news-item/NewsItem';
 import {
@@ -21,6 +21,34 @@ function App() {
     'new',
   );
   const [maxNewsItem, setMaxNewsItem] = useState(0);
+  const observer = useRef<IntersectionObserver>();
+
+  const actionInSight: IntersectionObserverCallback = useCallback(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        setLoading(true);
+        Promise.all(
+          newsIdList
+            .slice(news.length, news.length + LIMIT)
+            .map((id) => getNewsItem(id)),
+        ).then((items) => {
+          setNews([...news, ...items]);
+          setLoading(false);
+        });
+      }
+    },
+    [news, newsIdList],
+  );
+
+  const lastItem = useCallback(
+    (node: Element) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(actionInSight);
+
+      if (node) observer.current.observe(node);
+    },
+    [actionInSight],
+  );
 
   useEffect(() => {
     setLoading(true);
@@ -45,7 +73,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    Promise.all(newsIdList.slice(0, 30).map((id) => getNewsItem(id))).then(
+    Promise.all(newsIdList.slice(0, LIMIT).map((id) => getNewsItem(id))).then(
       (items) => {
         setNews(items);
         setLoading(false);
@@ -77,22 +105,44 @@ function App() {
   return (
     <div className={styles.main}>
       <Header handleSortButton={handleSort} activeButton={activeButton} />
-      {!loading && (
-        <ul className={styles.list}>
-          {news?.map((item, i) => (
+      <ul className={styles.list}>
+        {news?.map((item, i) => {
+          if (i + 1 === news.length) {
+            return (
+              <NewsItem
+                key={item.id}
+                serialNumber={i + 1}
+                by={item.by}
+                id={item.id}
+                score={item.score}
+                title={item.title}
+                url={item.url}
+                favorite={item.favorite}
+                descendants={item.descendants}
+                ref={lastItem}
+              />
+            );
+          }
+
+          return (
             <NewsItem
-              key={item.id}
+              key={item?.id}
               serialNumber={i + 1}
-              by={item.by}
-              id={item.id}
-              score={item.score}
-              title={item.title}
-              url={item.url}
-              favorite={item.favorite}
-              descendants={item.descendants}
+              by={item?.by}
+              id={item?.id}
+              score={item?.score}
+              title={item?.title}
+              url={item?.url}
+              favorite={item?.favorite}
+              descendants={item?.descendants}
             />
-          ))}
-        </ul>
+          );
+        })}
+      </ul>
+      {loading && (
+        <div className={styles['loader-block']}>
+          <span className={styles.loader}></span>
+        </div>
       )}
       {loading && <span className={styles.loader}></span>}
     </div>
